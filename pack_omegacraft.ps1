cc4fd32f86e564a063725cfc0aa8417867648d76
Omega9380 <omega9380@gmail.com>
2019-08-05 23:10:48 -0400

    Removed 3D items from Xumivoid
    Removed 3D doors/trapdoors/vines
    Updated pack_omegacraft.ps1
    Started working on a new model for the 3D curved rail

--- pack_omegacraft.ps1
+++ pack_omegacraft.ps1
@@ -1,37 +1,47 @@
 $time = Get-Date -UFormat %H%M%S
 $date = Get-Date -UFormat %Y%m%d
 $timestamp = $date + "T" + $time
-$destination = "Q:\games\Minecraft\textureedit\omegacraft14\backup_omegacraft14_" + $timestamp + ".zip"
+$zipfile = "omegacraft14.zip"
+$location = "Q:\games\Minecraft\textureedit"
+$zip = $location + "\packed\" + $zipfile
+$destination = $location + "\backup\backup_omegacraft14_" + $timestamp + ".zip"
+$working = $location + "\OmegaCraft-Resource-Pack\omegacraft14"
+$git = $location + "\OmegaCraft-Resource-Pack"
 
 # Backup resource pack
-	Write-Host "Backing up omegacraft14..."
-	Copy-Item -Path "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" -Destination $destination
-	Write-Host "Done."
-
-# Create new pack and move it
-	if (!(Test-Path "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip")) {
-		7z a "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" "Q:\games\Minecraft\textureedit\omegacraft14\OmegaCraft-Resource-Pack\omegacraft14\*"
+	if (Test-Path $zip){
+		Write-Host "Backing up $zip..."
+		Copy-Item -Path $zip -Destination $destination
+		Write-Host "Done."
 	}
 	else {
-		7z u -up0q3r2x2y2z1w2 "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" "Q:\games\Minecraft\textureedit\omegacraft14\OmegaCraft-Resource-Pack\omegacraft14\*"
+		Write-Host "$zip not found.  Creating a new one..."
+	}
+
+# Create new pack and move it
+	if (!(Test-Path "$location\packed\omegacraft14.zip")) {
+		7z a $zip "$working\*"
+	}
+	else {
+		7z u -up0q0r2x2y2z1w2 $zip "$working\*"
 	}
 	
-	Move-Item -Path "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" -Destination "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" -Force
+#	Move-Item -Path "$location\omegacraft14.zip" -Destination "$location\omegacraft14.zip" -Force
 
 # Calculate SHA1 hash for server deployment
-	$Hash = Get-FileHash -Algorithm SHA1 "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip"
+	$Hash = Get-FileHash -Algorithm SHA1 $zip
 	Write-Host "`nSHA1 Hash: " $Hash.Hash
 	Set-Clipboard -Value $Hash.Hash
 	Write-Host "`nSHA1 Hash copied to clipboard."
 
 # Copy resource pack to resource pack folder
-	Copy-Item "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" -Destination "$env:appdata\.minecraft\resourcepacks" -Force
-	Write-Host "`nCopied to Minecraft resourcepacks folder."
+	Copy-Item $zip -Destination "$env:appdata\.minecraft\resourcepacks" -Force
+	Write-Host "`nCopied $zip to Minecraft resourcepacks folder."
 
 # Send the resource pack to the server
-	scp -P 23466 "Q:\games\Minecraft\textureedit\omegacraft14\omegacraft14.zip" root@www:/var/www/snipeit/public/minecraft
-	ssh -p 23466 root@www 'chown -v snipeitapp:apache /var/www/snipeit/public/minecraft/omegacraft14.zip'
-	Write-Host "`nCopied to web server."
+	# scp -P 23466 $zip root@www:/var/www/snipeit/public/minecraft
+	# ssh -p 23466 root@www 'chown -v snipeitapp:apache /var/www/snipeit/public/minecraft/omegacraft14.zip'
+	# Write-Host "`nCopied to web server."
 
 Write-Host "`nPress any key to continue...";
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
